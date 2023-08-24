3b3a0c72129d171a8502460efe257ef51a5560f3
Scott Evans <git@scott.ee>
2023-08-24 10:08:23 +0200

    Add setting for managing CDN

--- src/_config/filters/cdnify.js
+++ src/_config/filters/cdnify.js
@@ -1,5 +1,5 @@
 module.exports = function cdnify(url) {
-    if (this.ctx.settings.isProduction) {
+    if (this.ctx.settings.isProduction && this.ctx.settings.cdn) {
         url = 'https://i0.wp.com/' + url.replace(/^https?:\/\//, '');
     }
     return url;
