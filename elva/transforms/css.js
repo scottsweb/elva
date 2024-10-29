b8908bf790876d2e762ef848a081e3cc575764d1
Scott Evans <git@scott.ee>
2024-10-29 11:42:46 +0100

    Switch to lightning css

--- elva/transforms/css.js
+++ elva/transforms/css.js
@@ -1,9 +1,23 @@
-import CleanCSS from 'clean-css';
+import { transform, Features, browserslistToTargets } from 'lightningcss';
+import browserslist from 'browserslist';
 
 export default eleventyConfig => {
     eleventyConfig.addTransform('css-minify', (content, path) => {
-        if (path && path.endsWith('.css') && eleventyConfig.globalData.settings.isProduction) {
-            return new CleanCSS().minify(content).styles;
+        if (path && path.endsWith('.css')) {
+            const css = transform({
+                code: Buffer.from(content),
+                minify: eleventyConfig.globalData.settings.isProduction,
+                sourceMap: false,
+                include: Features.Nesting,
+                drafts: { customMedia: true },
+                errorRecovery: true,
+                targets: browserslistToTargets(browserslist('> 0.2% and not dead'))
+            });
+
+            if (css?.warnings?.length) {
+                eleventyConfig.logger.error('CSS error: ' + css?.warnings[0]?.message);
+            }
+            return css.code.toString('utf8');
         }
         return content;
     });
