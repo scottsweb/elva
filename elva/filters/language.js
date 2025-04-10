fb18a01250ee242c942191a10bc033023a35898d
Scott Evans <git@scott.ee>
2025-04-10 12:27:54 +0200

    Defensive check before filtering collection

--- elva/filters/language.js
+++ elva/filters/language.js
@@ -1,5 +1,5 @@
 export default function languageFilter(collection, lang) {
     if (!lang) lang = this.page.lang || this.ctx.lang;
-    const filtered = collection.filter(item => item.page.lang == lang);
+    const filtered = collection?.filter(item => item.page.lang == lang);
     return filtered;
 }
