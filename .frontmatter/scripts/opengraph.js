178f2299dd3f02159eb627c483d75825e8157bd0
Scott Evans <git@scott.ee>
2025-04-29 12:42:45 +0200

    Make opengraph generation more robust

--- .frontmatter/scripts/opengraph.js
+++ .frontmatter/scripts/opengraph.js
@@ -5,16 +5,21 @@ import nodeHtmlToImage from 'node-html-to-image';
 const args = process.argv;
 const template = fs.readFileSync(args[1].replace('opengraph.js', 'opengraph-template.html'), 'utf8');
 const frontmatter = args[4] && typeof args[4] === "string" ? JSON.parse(args[4]) : null;
-const data = {...frontmatter, ...{ 'url': 'http://localhost:8080' }};
+let data = {...frontmatter, ...{ 'url': 'http://localhost:8080' }};
+
+// fix some potential encoding issues
+data.title && (data.title = decodeURIComponent(data.title));
+data.seo.title && (data.seo.title = decodeURIComponent(data.seo.title));
+data.seo.description && (data.seo.description = decodeURIComponent(data.seo.description));
 
 nodeHtmlToImage({
-    output: args[2] + '/content/assets/img/opengraph-' + slugify(data.title) + '.png',
+    output: args[2] + '/content/assets/img/opengraph-' + slugify(data.title, {decamelize: false}) + '.png',
     html: template,
     content: data
 }).then(() => {
     const output = JSON.stringify({
         'frontmatter': {
-            'thumbnail': '/assets/img/opengraph-' + slugify(data.title) + '.png'
+            'thumbnail': '/assets/img/opengraph-' + slugify(data.title, {decamelize: false}) + '.png'
         }
     });
     console.log(output);
