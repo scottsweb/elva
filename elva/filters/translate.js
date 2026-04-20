98c0c89956821c37f8f242ca518246380d449223
Scott Evans <git@scott.ee>
2026-04-20 10:28:11 +0200

    Bump 11ty to 4.x alpha

--- elva/filters/translate.js
+++ elva/filters/translate.js
@@ -2,7 +2,7 @@
 // can support data substitution, for example: {{ 'translation.key' | translate(page.lang, {data: 500}) }}
 // to-do: can this be made to support plurals also?
 import get from 'lodash.get';
-import nunjucks from 'nunjucks';
+import nunjucks from '@11ty/nunjucks';
 nunjucks.configure({ autoescape: true });
 
 export default function translate(lookup, lang, data = {}) {
