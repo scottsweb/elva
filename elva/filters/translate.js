47d80b20758f169c4423e4da52b36967f4c9ce7c
Scott Evans <git@scott.ee>
2026-06-05 15:08:51 +0200

    Remove lodash dependency

--- elva/filters/translate.js
+++ elva/filters/translate.js
@@ -1,12 +1,13 @@
 // finds a translation from content/_data/translations.js
 // can support data substitution, for example: {{ 'translation.key' | translate(page.lang, {data: 500}) }}
 // to-do: can this be made to support plurals also?
-import get from 'lodash.get';
 import nunjucks from '@11ty/nunjucks';
 nunjucks.configure({ autoescape: true });
 
 export default function translate(lookup, lang, data = {}) {
     if (!lang) lang = this.page.lang || this.ctx.lang;
-    const translation = nunjucks.renderString(get(this.ctx.translations[lang], `[${lookup}]`), data);
+    // traverse the translations object using the dot-separated lookup path, then render as a Nunjucks template so variables like {data: 500} get substituted in
+    const value = lookup.split('.').reduce((o, k) => o?.[k], this.ctx.translations[lang]);
+    const translation = nunjucks.renderString(value, data);
     return translation;
 }
