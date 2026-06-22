d3c047c0488a512c363904bc308ed7a83441d596
Scott Evans <git@scott.ee>
2026-06-22 14:54:45 +0200

    Remove alpine.js Intersect, use CSS for scroll tracking example

--- themes/default/js/scripts.js
+++ themes/default/js/scripts.js
@@ -2,7 +2,6 @@ document.addEventListener('alpine:init', () => {
     Alpine.store('elva', {
         init() {
             this.theme = localStorage.getItem('theme') === null ? window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light' : localStorage.getItem('theme')
-            this.scrollPosition = window.scrollY
 
             document.querySelectorAll('[loading="lazy"]').forEach(element => {
                 const animateIn = () => {
@@ -12,15 +11,9 @@ document.addEventListener('alpine:init', () => {
             });
         },
         theme: null,
-        scrollPosition: 0,
-        scrollPercent: 0,
         themeToggle() {
             (this.theme === 'light') ? this.theme = 'dark' : this.theme = 'light'
             localStorage.setItem('theme', this.theme)
-        },
-        scrollPositionUpdate() {
-            this.scrollPosition = window.scrollY
-            this.scrollPercent = Math.round(((document.body.scrollTop || document.documentElement.scrollTop) / ( document.documentElement.scrollHeight - document.documentElement.clientHeight )) * 100)
         }
     })
 })
