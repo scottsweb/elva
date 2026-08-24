1ab169659f035672704d9f05a6b58db27ab93d73
Scott Evans <git@scott.ee>
2026-08-24 13:46:41 +0200

    Inline docs fix

--- elva/filters/tagged.js
+++ elva/filters/tagged.js
@@ -1,5 +1,5 @@
 // filter collection by a tag or number of tags
-// {% set collection = collections._posts | tagged(['tag1', 'tag2']) %}
+// {% set collection = collections._posts | tags(['tag1', 'tag2']) %}
 export function tagged(collections, tags = []) { 
     return collections.filter((item) => {
         const data = item && item.data ? item.data : item;
